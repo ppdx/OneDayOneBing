4d5ce1a68fc8599e3f64e410ec4e82c7554d3a5c
ppdxxm <ppdxxm@outlook.com>
2017-05-27 20:54:32 +0800

    修改.net 到 2.0

--- OneDayOneBing/Form1.cs
+++ OneDayOneBing/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;  //调用WINDOWS API函数时要用到
 using Microsoft.Win32;  //写入注册表时要用到
